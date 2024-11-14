671772ebea54e5e5cb23932f5d24ed3ef11968e3
erkalai <er.kalai.v@gmail.com>
2024-11-13 23:27:12 -0800

    Add Contact Details & Map

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -109,7 +109,7 @@ const ContactForm = () => {
           <h2>Our Location</h2>
           <iframe
             title="Google Map"
-            src="https://www.google.com/maps/embed?pb=YOUR_GOOGLE_MAPS_EMBED_URL"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3058.4493818317624!2d79.64144327369375!3d10.629832361883322!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a5545245457383b%3A0x91b67e2e58454965!2sMUTHAMIZH%20AUDIOS!5e1!3m2!1sen!2sin!4v1731568140832!5m2!1sen!2sin"
             width="100%"
             height="450"
             style={{ border: 0 }}
@@ -119,12 +119,12 @@ const ContactForm = () => {
         </div>
       </div>
 
-      {/* Contact Information (Email, Mobile, and Address) */}
       <div id="contact-info">
         <h2>Contact Information</h2>
-        <p>Email: <a href="mailto:info@yourwebsite.com">info@yourwebsite.com</a></p>
-        <p>Phone: <a href="tel:+1234567890">+123 456 7890</a></p>
-        <p>Mobile: <a href="tel:+12345678901">+123 456 78901</a></p>
+        <p>Email: <a href="mailto:muthamizhaudios@gmail.com">muthamizhaudios@gmail.com</a></p>
+        <p>Email: <a href="mailto:tamizh3500@gmail.com">tamizh3500@gmail.com</a></p>
+        <p>Mobile: <a href="tel:+918098353500">+91 80983 53500</a></p>
+        <p>Mobile: <a href="tel:+918098887469">+91 80988 87469</a></p>
       </div>
     </div>
     </section>
